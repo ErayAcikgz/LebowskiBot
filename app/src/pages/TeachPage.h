144b00ba2fce94b3a27a658b27052ced4f337efb
furkankuyumcu54 <kuyumcufurkan05@gmail.com>
2026-09-11 19:06:20 +0300

    Implement Teach page UI

--- app/src/pages/TeachPage.h
+++ app/src/pages/TeachPage.h
@@ -1,14 +1,37 @@
 #ifndef LEBOWSKI_TEACHPAGE_H
 #define LEBOWSKI_TEACHPAGE_H
 
-#include "PlaceholderPage.h"
+#include <QVector>
+#include <QWidget>
+#include <QString>
 
-class TeachPage final : public PlaceholderPage
+class QLabel;
+class QListWidget;
+class QLineEdit;
+
+class TeachPage final : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit TeachPage(QWidget *parent = nullptr);
+
+private:
+    struct Waypoint {
+        QString name;
+    };
+
+    void refreshWaypointList();
+    void selectWaypoint(int index);
+    void captureWaypoint();
+    void renameWaypoint();
+    void deleteWaypoint();
+
+    QVector<Waypoint> waypoints_;
+    int selectedWaypoint_ = -1;
+    QLineEdit *waypointNameInput_ = nullptr;
+    QListWidget *waypointList_ = nullptr;
+    QLabel *selectedWaypointLabel_ = nullptr;
 };
 
 #endif // LEBOWSKI_TEACHPAGE_H
